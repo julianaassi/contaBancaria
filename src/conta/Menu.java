6c11ccc2b36e2a738fab6de54228a645b043242a
juliana_assi <juliana-3@hotmail.com>
2025-02-12 15:56:43 -0300

    atualização conta bancária- model conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,39 +1,47 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
 	
-	Scanner leia= new Scanner(System.in);
+		// Teste da Classe Conta
+				Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+				c1.visualizar();
+				c1.sacar(12000.0f);
+				c1.visualizar();
+				c1.depositar(5000.0f);
+				c1.visualizar();
+
+				Scanner leia = new Scanner(System.in);
 	
 	int opcao;
 	
 	while (true) {
 		
 		System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
-		                  + "*********************************************");
-		System.out.println("                                             ");
-		System.out.println("                BANCO DO BRAZIL COM Z        ");     
-		System.out.println("                                             ");
-		System.out.println("*********************************************");
-		System.out.println("                                             ");
-		System.out.println("           1- CRIAR UMA CONTA                ");
-		System.out.println("           2- LISTAR TODAS AS CONTAS         ");
-		System.out.println("           3- BUSCAR CONTA POR NÚMERO        ");
-		System.out.println("           4- ATUALIZAR DADOS DA CONTA       ");
-		System.out.println("           5- APAGAR CONTA                   ");
-		System.out.println("           6- SACAR                          ");
-		System.out.println("           7- DEPOSITAR                      ");
-		System.out.println("           8- TRANSFERIR VALORES ENTRE CONTAS");
-		System.out.println("           9- SAIR                           ");
-		System.out.println("                                             ");
-		System.out.println("*********************************************");
-		System.out.println("   Entre com a opção desejada:               ");
-		System.out.println("                                             "+ Cores.TEXT_RESET);
+		                  + "************************************************");
+		System.out.println("                                                 ");
+		System.out.println("                BANCO DO BRAZIL COM Z            ");     
+		System.out.println("                                                 ");
+		System.out.println("*************************************************");
+		System.out.println("                                                 ");
+		System.out.println("           1- CRIAR UMA CONTA                    ");
+		System.out.println("           2- LISTAR TODAS AS CONTAS             ");
+		System.out.println("           3- BUSCAR CONTA POR NÚMERO            ");
+		System.out.println("           4- ATUALIZAR DADOS DA CONTA           ");
+		System.out.println("           5- APAGAR CONTA                       ");
+		System.out.println("           6- SACAR                              ");
+		System.out.println("           7- DEPOSITAR                          ");
+		System.out.println("           8- TRANSFERIR VALORES ENTRE CONTAS    ");
+		System.out.println("           9- SAIR                               ");
+		System.out.println("                                                 ");
+		System.out.println("*************************************************");
+		System.out.println("   Entre com a opção desejada:                   ");
+		System.out.println("                                                 "+ Cores.TEXT_RESET);
 		
 		opcao = leia.nextInt();
 		if(opcao ==9) {
